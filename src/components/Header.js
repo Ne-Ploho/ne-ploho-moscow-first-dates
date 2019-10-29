04df90423a7539c9af730e69bb072cd12c4f031c
Kuzya <alexkuz@gmail.com>
2019-10-29 18:55:18 +0300

    add lingui

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,15 @@
 import * as React from 'react';
 import styled from 'styled-components';
 import { Link } from 'gatsby';
+import { Trans } from '@lingui/macro';
 
-const Header = ({  }) => {
+const Header = ({ lang, onLangChange }) => {
   return <HeaderRoot>
-    <Title>КАРТА ПЕРВЫХ СВИДАНИЙ МОСКВЫ / FIRST DATES OF MOSCOW</Title>
-    <AboutLink to="/about">О КАРТЕ / ABOUT</AboutLink>
+    <Title><Trans>First dates of Moscow</Trans></Title>
+    <Right>
+      <AboutLink to="/about"><Trans>About</Trans></AboutLink>
+      <LangSwitch onClick={() => onLangChange(lang === 'ru' ? 'en' : 'ru')}>{lang === 'ru' ? 'EN' : 'RU'}</LangSwitch>
+    </Right>
   </HeaderRoot>;
 };
 
@@ -14,7 +18,7 @@ export default Header;
 const HeaderRoot = styled.header`
   height: 40px;
   flex: 0 0 auto;
-  padding: 10px 24px;
+  padding: 8px 24px;
   box-sizing: border-box;
   display: flex;
   justify-content: space-between;
@@ -25,12 +29,28 @@ const Title = styled.h1`
   margin: 0;
   font-weight: normal;
   font-size: 1em;
+  text-transform: uppercase;
 `;
 
 const AboutLink = styled(Link)`
   text-decoration: none;
+  text-transform: uppercase;
 
   &:hover {
     text-decoration: underline;
   }
+`;
+
+const LangSwitch = styled.button`
+  border: 0;
+  background: #EB212E;
+  color: #FFFFFF;
+  padding: 6px 8px;
+  cursor: pointer;
+  margin-left: 16px;
+  line-height: 1;
+`;
+
+const Right = styled.div`
+  display: flex;
 `;
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -3,21 +3,38 @@ import styled from 'styled-components';
 import DatesMap from '../components/DatesMap';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import { I18nProvider } from '@lingui/react';
+
+import catalogEn from '../locales/en/messages.js'
+import catalogRu from '../locales/ru/messages.js'
+
+const catalogs = { en: catalogEn, ru: catalogRu };
+
+function getDefaultLang() {
+  return 'ru';
+}
 
 const Index = ({ children, data, pageContext }) => {
   const edges = data && data.allContentfulStory ? data.allContentfulStory.edges : [];
+  const [lang, setLang] = React.useState(getDefaultLang());
 
   const stories = edges.map(e => e.node);
 
+  const updateLang = lang => {
+    setLang(lang);
+  }
+
   return (
-  <IndexRoot>
-    <Header fromYear={pageContext.fromYear} toYear={pageContext.toYear} />
-    <Main>
-      <DatesMap stories={stories.filter(s => s.node_locale === 'ru-RU')} />
-      {children}
-    </Main>
-    <Footer fromYear={pageContext.fromYear} toYear={pageContext.toYear} />
-  </IndexRoot>
+  <I18nProvider language={lang} catalogs={catalogs}>
+    <IndexRoot>
+      <Header fromYear={pageContext.fromYear} toYear={pageContext.toYear} lang={lang} onLangChange={updateLang} />
+      <Main>
+        <DatesMap stories={stories.filter(s => s.node_locale === 'ru-RU')} />
+        {children}
+      </Main>
+      <Footer fromYear={pageContext.fromYear} toYear={pageContext.toYear} />
+    </IndexRoot>
+  </I18nProvider>
   );
 };
 
