2f1017dd328a490a5afe183ecf2c67277fb79da8
Kuzya <alexkuz@gmail.com>
2019-10-31 13:31:54 +0300

    ignore export data

--- scripts/export.sh
+++ scripts/export.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
+mkdir -p export-data
+
 ./node_modules/.bin/contentful space export --space-id="$CONTENTFUL_SPACE_ID" --mt="$CONTENTFUL_MANAGEMENT_TOKEN" --export-dir="export-data" --content-only
