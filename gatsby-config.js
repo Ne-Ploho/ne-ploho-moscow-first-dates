acc7459d4188430fcbc4fd19e8515fc9691e3af0
Kuzya <alexkuz@gmail.com>
2019-11-08 11:21:32 +0300

    add nprogress

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,19 +1,19 @@
 require('dotenv').config({
   path: `.env.${process.env.NODE_ENV}`
-})
+});
 
 const contentfulConfig = {
   spaceId: process.env.CONTENTFUL_SPACE_ID,
   accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
   host: process.env.CONTENTFUL_HOST
-}
+};
 
-const { spaceId, accessToken } = contentfulConfig
+const { spaceId, accessToken } = contentfulConfig;
 
 if (!spaceId || !accessToken) {
   throw new Error(
     'Contentful spaceId and the access token need to be provided.'
-  )
+  );
 }
 
 module.exports = {
@@ -22,31 +22,37 @@ module.exports = {
     'gatsby-plugin-layout',
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-react-svg',
-     'gatsby-transformer-sharp',
-     'gatsby-plugin-sharp',
-     'gatsby-transformer-remark',
-     {
-       resolve: 'gatsby-source-contentful',
-       options: contentfulConfig,
-     },
-     {
-       resolve: 'gatsby-plugin-google-fonts',
-       options: {
-         fonts: ['Montserrat:400,700'],
-         display: 'fallback'
-       }
-     },
-     'gatsby-plugin-styled-components',
-     {
-       resolve: `gatsby-plugin-manifest`,
-       options: {
-         name: `Карта первых свиданий Москвы`,
-         short_name: `Карта первых свиданий Москвы`,
-         start_url: `/`,
-         background_color: `#FFFFFF`,
-         theme_color: `#FA61B6`,
-         display: `standalone`,
-       },
-     }
-  ],
-}
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-remark',
+    {
+      resolve: 'gatsby-source-contentful',
+      options: contentfulConfig
+    },
+    {
+      resolve: 'gatsby-plugin-google-fonts',
+      options: {
+        fonts: ['Montserrat:400,700'],
+        display: 'fallback'
+      }
+    },
+    'gatsby-plugin-styled-components',
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Карта первых свиданий Москвы`,
+        short_name: `Карта первых свиданий Москвы`,
+        start_url: `/`,
+        background_color: `#FFFFFF`,
+        theme_color: `#FA61B6`,
+        display: `standalone`
+      }
+    },
+    {
+      resolve: 'gatsby-plugin-nprogress',
+      options: {
+        color: '#EB212E'
+      }
+    }
+  ]
+};
